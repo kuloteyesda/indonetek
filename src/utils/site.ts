aec618b0ea09692fbf840bfdc48109b06c3e3a25
kuloteyesda <kuloteyesda@gmail.com>
2025-06-30 16:03:05 +0700

    Add files via upload

--- src/utils/site.ts
+++ src/utils/site.ts
@@ -1,4 +1,4 @@
 export const nama = 'Indo Netek';
-export const desk = 'Video bokep indo jepang viral ngentot colmek bocil jilbab smp tante.';
+export const desk = 'Video Bokep Indo Jepang Jav Hd Artis Viral Terbaru Bocil Ngentot Jilbab Smp Skandal';
 export const url = 'https://indonetek.pages.dev';
-export const terbit = '2025-06-26T00:00:00Z';
+export const terbit = '2025-06-26T00:01:03Z';
