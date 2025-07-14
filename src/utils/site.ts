b21b2476b476dc553105341aae2aa1cc8a3afa7f
kuloteyesda <kuloteyesda@gmail.com>
2025-07-15 00:34:55 +0700

    Delete src directory

--- src/utils/site.ts
+++
@@ -1,4 +0,0 @@
-export const nama = 'Indo Netek';
-export const desk = 'Video Bokep Indo Jepang Jav Hd Artis Viral Terbaru Bocil Ngentot Jilbab Smp Skandal';
-export const url = 'https://indonetek.pages.dev';
-export const terbit = '2025-06-26T00:01:03Z';
